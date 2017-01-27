c6904cf26015b59ebff6426fb19a6e98e4cb2713
Brendan Pete <brendan.m.pete@gmail.com>
2017-01-27 16:22:08 -0500

    Mystery destroy path

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,9 +2,14 @@ class PostsController < ApplicationController
   def create
     @location = Location.find(params[:location_id])
     @post = @location.posts.create(post_params)
-
     redirect_to location_path(@location)
+  end
 
+  def destroy
+    @location = Location.find(params[:location_id])
+    @post = @location.posts.find(params[:id])
+    @post.destroy
+    redirect_to location_path(@location)
   end
 
   private
