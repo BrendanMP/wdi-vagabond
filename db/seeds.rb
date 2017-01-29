e52e9efdbd7f31c06d2a50bb3661ea687c77f82d
Lisa Holter <lisaholter@Lisas-MacBook-Air.local>
2017-01-28 23:18:40 -0500

    contentfix

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,7 +8,7 @@
    Location.create(name: 'Boston', photo_url: "http://www.bostonlogic.com/wp-content/uploads/2015/03/photodune-2705436-boston-waterfront-m1.jpg")
    Location.create(name: 'New York', photo_url: "http://mustseeplaces.eu/wp-content/uploads/2016/08/new-york-skyline.jpg")
    Location.create(name: 'Wellington', photo_url: "http://vizts.com/wp-content/uploads/2015/10/a-beautiful-view-of-Wellington.jpg")
-   Location.create(name: 'San Fransico', photo_url: "https://lonelyplanetimages.imgix.net/a/g/hi/t/9cf024dfd5c0bcb2b17f4785340145ea-san-francisco.jpg?sharp=10&vib=20&w=1200")
+   Location.create(name: 'San Francisco', photo_url: "https://lonelyplanetimages.imgix.net/a/g/hi/t/9cf024dfd5c0bcb2b17f4785340145ea-san-francisco.jpg?sharp=10&vib=20&w=1200")
    Location.create(name: 'London', photo_url: "https://brightcove04pmdo-a.akamaihd.net/5104226627001/5104226627001_5232370632001_5203581082001-vs.jpg?pubId=5104226627001&videoId=5203581082001")
    Location.create(name: 'Dubai', photo_url: "http://ihg.scene7.com/is/image/ihg/intercontinental-dubai-4328452913-2x1?wid=1440&fit=fit,1")
    Location.create(name: 'Tokyo', photo_url: "https://voulezvousdiner.a.cdnify.io/photos/_places/places.tokyo.jpg")
